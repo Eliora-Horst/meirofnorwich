3533e2a902a4fd02f5afe7a5f0c0f8b7fdc601e2
Eliora Horst <eliora.horst@gmail.com>
2020-05-31 19:40:15 +0100

    Trying to get CSS to load

--- app.js
+++ app.js
@@ -17,24 +17,49 @@ app.get('/', function(req, res){
   res.sendFile('/HTML/meirMain.html', {root: './public'})
 });
 
+//The Poems
 app.get('/poems', function(req, res){
 	console.log(res.sendFile)
   res.sendFile('/HTML/thePoems.html', {root: './public'})
 });
 
+//The Poet
 app.get('/poet', function(req, res){
 	console.log(res.sendFile)
   res.sendFile('/HTML/thePoet.html', {root: './public'})
 });
 
+//The Manuscript
 app.get('/manuscript', function(req, res){
 	console.log(res.sendFile)
   res.sendFile('/HTML/theManuscript.html', {root: './public'})
 });
 
+//About Page
 app.get('/about', function(req, res){
 	console.log(res.sendFile)
   res.sendFile('/HTML/about.html', {root: './public'})
+});
+
+//Poems
+app.get('/On-the-Termination-of-the-Sabbath', function(req, res){
+	console.log(res.sendFile)
+  res.sendFile('/HTML/poems/OnTheTerminationOfTheSabbath.html', {root: './public'})
+});
+
+app.get('/Put-a-Curse-On-My-Enemy', function(req, res){
+	console.log(res.sendFile)
+  res.sendFile('/HTML/poems/PutACurseOnMyEnemy.html', {root: './public'})
+});
+
+app.get('/Who-Is-Like-You', function(req, res){
+	console.log(res.sendFile)
+  res.sendFile('/HTML/poems/WhoIsLikeYou.html', {root: './public'})
+});
+
+app.get('/Sixteen-Short-Poems', function(req, res){
+	console.log(res.sendFile)
+  res.sendFile('/HTML/poems/16ShortPoems.html', {root: './public'})
 });
 
 var port = 80
